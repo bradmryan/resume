714b7ac9bfc1d54a80b2a63830914de1da532453
Brad Ryan <brad.m.ryan@gmail.com>
2016-08-19 16:04:53 -0400

    Formatted skill keywords for resumes

--- main/models.py
+++ main/models.py
@@ -128,7 +128,7 @@ class Skill(models.Model):
 
 
 class Keyword(models.Model):
-    word = models.CharField(max_length=25)
+    word = models.CharField(max_length=50)
 
     def __str__(self):
         return self.word
