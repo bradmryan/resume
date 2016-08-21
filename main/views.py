2aab3c1eaf7d4996056b6070ea4044071ff8bda4
Brad Ryan <brad.m.ryan@gmail.com>
2016-08-21 12:22:01 -0400

    consolidate get resume and get json function

--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
 from django.conf import settings
+from django.views.decorators.http import require_GET
 
 import weasyprint
 
@@ -12,6 +13,7 @@ from .models import Resume, Profile, Work, WorkHighlight, Education, Course, Awa
 
 
 # Create your views here.
+@require_GET
 def home(req):
     context = {}
     email = "brad.m.ryan@gmail.com"
@@ -23,8 +25,7 @@ def home(req):
 
     return render(req, 'main/home.html', context)
 
-
-def get_resume_json(req):
+def get_resume(email, format_phone=False):
     resume = get_object_or_404(Resume, email="brad.m.ryan@gmail.com")
 
     resumes_dict = {}
@@ -52,6 +53,11 @@ def get_resume_json(req):
     references = Reference.objects.filter(resume=resume)
 
     name = ' '.join([resume.firstname, resume.middleinitial, resume.lastname])
+
+    if format_phone:
+        phone = '(' + resume.phone[:3] + ') ' + resume.phone[3:6] + '-' + resume.phone[6:10]
+    else:
+        phone = resume.phone
 
     for profile in profiles:
         network = profile.get_network_display()
@@ -128,7 +134,7 @@ def get_resume_json(req):
         reference_dict = {"name": reference.name, "reference": reference.reference}
         reference_rec.append(reference_dict)
 
-    resume_dict["basics"] = { "name": name.title(), "label": resume.label, "picture": resume.picture.url, "email": resume.email, "phone": resume.phone, "website": resume.website, "summary": resume.summary }
+    resume_dict["basics"] = { "name": name.title(), "label": resume.label, "picture": resume.picture.url, "email": resume.email, "phone": phone, "website": resume.website, "summary": resume.summary }
     resume_dict["basics"]["location"] = { "address": resume.address, "postalcode": resume.postalcode, "city": resume.city, "countrycode": resume.countrycode, "region": resume.region }
     resume_dict["basics"]["profiles"] = profile_rec
     resume_dict["volunteer"] = volunteer_rec
@@ -141,128 +147,18 @@ def get_resume_json(req):
     resume_dict["interests"] = interest_rec
     resume_dict["references"] = reference_rec
 
+    return resume_dict
+
+@require_GET
+def get_resume_json(req):
+    resume_dict = get_resume("brad.m.ryan@gmail.com")
+
     return JsonResponse(resume_dict)
 
-
+@require_GET
 def get_resume_pdf(req):
-    resume = get_object_or_404(Resume, email="brad.m.ryan@gmail.com")
     template = get_template('main/resume.html')
-
-    resumes_dict = {}
-    resumes_rec = []
-    resume_dict = {}
-    profile_rec = []
-    work_rec = []
-    volunteer_rec = []
-    education_rec = []
-    award_rec = []
-    publication_rec = []
-    skill_rec = []
-    language_rec = []
-    interest_rec = []
-    reference_rec = []
-
-    profiles = Profile.objects.filter(resume=resume)
-    works = Work.objects.filter(resume=resume).order_by('-startdate')
-    education = Education.objects.filter(resume=resume).order_by('-startdate')
-    awards = Award.objects.filter(resume=resume)
-    publications = Publication.objects.filter(resume=resume)
-    skills = Skill.objects.filter(resume=resume).order_by('name')
-    languages = Language.objects.filter(resume=resume)
-    interests = Interest.objects.filter(resume=resume).order_by('name')
-    references = Reference.objects.filter(resume=resume)
-
-    name = ' '.join([resume.firstname, resume.middleinitial + '.', resume.lastname])
-    phone = '(' + resume.phone[:3] + ') ' + resume.phone[3:6] + '-' + resume.phone[6:10]
-
-    for profile in profiles:
-        network = profile.get_network_display()
-        profile_dict = {"network": network, "username": profile.username, "url": profile.url}
-        profile_rec.append(profile_dict)
-
-    for work in works:
-        highlight_rec = []
-
-        highlights = WorkHighlight.objects.filter(work=work)
-
-        for highlight in highlights:
-            highlight_rec.append(highlight.highlight)
-
-        work_dict = {"position": work.position, "website": work.website, "startDate": work.startdate, "endDate": work.enddate, "summary": work.summary}
-
-        work_dict["highlights"] = highlight_rec
-
-        if work.volunteer:
-            work_dict["organization"] = work.company
-            volunteer_rec.append(work_dict)
-        else:
-            work_dict["company"] = work.company
-            work_rec.append(work_dict)
-
-    for edu in education:
-        course_rec = []
-
-        courses = Course.objects.filter(education=edu)
-
-        for course in courses:
-            record = " - ".join([course.coursecode, course.description ])
-            course_rec.append(record)
-
-        edu_dict = {"institution": edu.institution, "area": edu.area, "studyType": edu.studytype, "startDate": edu.startdate, "endDate": edu.enddate, "gpa": edu.gpa}
-        edu_dict["courses"] = course_rec
-        education_rec.append(edu_dict)
-
-    for award in awards:
-        award_dict = {"title": award.title, "date": award.date, "awarder": award.awarder, "summary": award.summary}
-        award_rec.append(award_dict)
-
-    for publication in publications:
-        publication_dict = {"name": publication.name, "publisher": publication.publisher, "releaseDate": publication.releasedate, "website": publication.website, "summary": publication.summary}
-        publication_rec.append(publication_dict)
-
-    for skill in skills:
-        keyword_rec = []
-
-        keywords = SkillKeyword.objects.filter(skill=skill)
-
-        for keyword in keywords:
-            keyword_rec.append(keyword.keyword)
-
-        skill_dict = {"name": skill.name, "level": skill.level, "keywords": keyword_rec}
-        skill_rec.append(skill_dict)
-
-    for language in languages:
-        language_dict = {"name": language.name, "level": language.level}
-        language_rec.append(language_dict)
-
-    for interest in interests:
-        keyword_rec = []
-
-        keywords = InterestKeyword.objects.filter(interest=interest)
-
-        for keyword in keywords:
-            keyword_rec.append(keyword.keyword.word)
-
-        interest_dict = {"name": interest.name, "keyword": keyword_rec}
-        interest_rec.append(interest_dict)
-
-    for reference in references:
-        reference_dict = {"name": reference.name, "reference": reference.reference}
-        reference_rec.append(reference_dict)
-
-    resume_dict["basics"] = { "name": name.title(), "label": resume.label, "picture": resume.picture.url, "email": resume.email, "phone": phone, "website": resume.website, "summary": resume.summary }
-    resume_dict["basics"]["location"] = { "address": resume.address, "postalcode": resume.postalcode, "city": resume.city, "countrycode": resume.countrycode, "region": resume.region }
-    resume_dict["basics"]["profiles"] = profile_rec
-    resume_dict["volunteer"] = volunteer_rec
-    resume_dict["work"] = work_rec
-    resume_dict["education"] = education_rec
-    resume_dict["awards"] = award_rec
-    resume_dict["publications"] = publication_rec
-    resume_dict["skills"] = skill_rec
-    resume_dict["language"] = language_rec
-    resume_dict["interests"] = interest_rec
-    resume_dict["references"] = reference_rec
-
+    resume_dict = get_resume("brad.m.ryan@gmail.com", format_phone=True)
     html = template.render(RequestContext(req, resume_dict))
     pdf = weasyprint.HTML(string=html).write_pdf()
     res = HttpResponse(pdf, content_type="application/pdf")
