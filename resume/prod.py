3b1ced487952416964a78195924b9bcfb6cdc79e
Brad Ryan <brad.m.ryan@gmail.com>
2016-08-25 15:08:37 -0400

    Added Restfull framework

--- resume/prod.py
+++ resume/prod.py
@@ -111,3 +111,11 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
